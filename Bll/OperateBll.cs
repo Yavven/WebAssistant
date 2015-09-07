ce46bc004e7cd890acaff54337a26ed1e426d1f6
Yawen <yavven@gmail.com>
2015-09-07 17:58:12 +0800

    修改了项目名称和函数名称

--- Bll/OperateBll.cs
+++ Bll/OperateBll.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace WebAutoOperator.Bll
+namespace WebAssistant.Bll
 {
     public class OperateBll
     {
--- Bll/UrlBll.cs
+++ Bll/UrlBll.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace WebAutoOperator.Bll
+namespace WebAssistant.Bll
 {
     public class UrlBll
     {
